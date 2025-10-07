3664bed7420bd87de69e13e39df538c239992acb
Dima <graiwers@gmail.com>
2025-10-07 17:47:58 +0400

    07

--- test_ui/test_SwagLabs.py
+++ test_ui/test_SwagLabs.py
@@ -1,57 +1,72 @@
+from pytest import mark, fixture
 from playwright.sync_api import Page, expect
 import os
 import dotenv
 from time import sleep
+import re
 
 dotenv.load_dotenv(override=True)
 
+# pytest --headed -s -m hm
 
-def test_full_correct(page: Page):
-    page.goto(os.getenv('site'))
+@fixture
+def login(page: Page):
+    def _login():
+        page.goto(os.getenv('site'))
+
+        page.fill('#user-name', os.getenv('user_correct'))
+  
+        page.fill('#password', os.getenv('passwd'))
+
+        page.click('#login-button')
+    return _login
+
+@mark.full
+def test_full_correct(page: Page, login):
+    login()
     
-    login = page.locator('#user-name')
-    login.fill(os.getenv('user_correct'))
-    
-    password =  page.locator('#password')
-    password.fill(os.getenv('passwd'))
-    
-    page.locator('#login-button').click()
-    
-    filter = page.locator('//*[@class="product_sort_container"]')
-    filter.select_option('Price (high to low)')
-    
+    page.select_option('//*[@class="product_sort_container"]', 'Price (high to low)')
+   
     page.get_by_text('Add to cart').nth(0).click()
     
-    page.locator('#shopping_cart_container').click()
+    page.click('#shopping_cart_container')
    
-    page.locator('#checkout').click()
+    page.click('#checkout')
     
-    page.locator('#first-name').fill('Pupkin')
-    page.locator('#last-name').fill('Ivan')
-    page.locator('#postal-code').fill('800-333-343-35')
+    page.fill('#first-name', 'Pupkin')
+    page.fill('#last-name', 'Ivan')
+    page.fill('#postal-code', '800-333-343-35')
     
-    page.locator('#continue').click()
+    page.click('#continue')
     
-    page.locator('#finish').click()    
+    page.click('#finish')    
 
-
-def test_currect_login(page: Page):
+@mark.smoke
+def test_correct_login(page: Page):
     page.goto(os.getenv('site'))
-    login = page.locator('#user-name')
-    login.fill(os.getenv('user_correct'))
-    password =  page.locator('#password')
-    password.fill(os.getenv('passwd'))
-    page.locator('#login-button').click()
+
+    page.fill('#user-name', os.getenv('user_correct'))
+  
+    page.fill('#password', os.getenv('passwd'))
+
+    page.click('#login-button')
     
     expect(page).to_have_url('https://www.saucedemo.com/inventory.html')
     
-
-def test_uncurrect_login(page: Page):
+@mark.smoke
+def test_incorrect_login(page: Page):
     page.goto(os.getenv('site'))
-    login = page.locator('#user-name')
-    login.fill(os.getenv('user_lock'))
-    password =  page.locator('#password')
-    password.fill(os.getenv('passwd'))
-    page.locator('#login-button').click()
+
     
-    expect(page).to_have_url('**/inventory.html', message="Переход не произошел")
+    page.fill('#user-name', os.getenv('user_lock'))
+
+    page.fill('#password', os.getenv('passwd'))
+
+    page.click('#login-button')
+    
+
+    expect(page).to_have_url(os.getenv('site'))
+
+    error_mess = page.locator('[data-test="error"]')
+    expect(error_mess).to_be_visible()
+
