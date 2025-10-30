4e1915612f659e0c7fef364c905b11fcf28bc2f5
My <graiwers@gmail.com>
2025-10-30 13:49:01 +0400

    30.10

--- test_git_api/test_git_api.py
+++ test_git_api/test_git_api.py
@@ -1,83 +1,67 @@
 import requests
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 git_token = os.getenv('git_token')
 git_name = os.getenv('git_name')
 git_repo = os.getenv('git_repo')
 
-url_get_list_repo = f"https://api.github.com/users/{git_name}/repos"
-url_create_repo = 'https://api.github.com/user/repos'
-url_delete_repo = f"https://api.github.com/repos/{git_name}/{git_repo}"
 
+url = 'https://api.github.com/'
 
-
-def new_repo(git_token, git_repo):
-    
-    headers = {
+headers = {
        "Authorization": f"token {git_token}",
        "Accept": "application/vnd.github.v3+json"
     }  
-        
+
+
+def test_get_repo():
+    
+    url_get_repo = f'{url}/users/{git_name}/repos'
+    
+    response = requests.get(url_get_repo, headers=headers)
+    
+    assert  response.status_code == 200, f'Ошибка: {response.status_code}'   
+    repos = response.json()
+    repo_names = [repo['name'] for repo in repos]
+    print("Список репозиториев:")
+    for name in repo_names:
+        print(f"  - {name}")
+    
+
+def test_new_repo():
+      
     data = {
         "name": git_repo,
         "description": None,
     }
-    
-        
+    url_create_repo = f'{url}/user/repos'
+            
     response = requests.post(url_create_repo, headers=headers, json=data)
-
-    print(f"Status Code: {response.status_code}")
-    
+   
     if response.status_code == 201:
         repo_data = response.json()
-        print("New public repo created successfully!")
+        print(f'Новый репозиторий {git_repo} успешно создан')
+        test_get_repo()  
         return repo_data
     elif response.status_code == 422:
-        print('Repository already exists')
+        print('Такой репозиторий уже есть')
         return None
     else:
-        print('Some error')
+        print(f'Ошибка: {response.status_code}')
         return None
+        
     
-def delete_repo(git_token):
-     
-    headers = {
-       "Authorization": f"token {git_token}",
-       "Accept": "application/vnd.github.v3+json"
-    }  
+    
+def test_delete_repo():
+    url_delete_repo = f'{url}/repos/{git_name}/{git_repo}'   
     
     response = requests.delete(url_delete_repo, headers=headers)
 
     if response.status_code == 204:
-        print(f"Repository has been deleted successfully. Status code: {response.status_code}")
+        print(f'Репозиторий {git_repo} успешно  удален')
+        test_get_repo()
     elif response.status_code == 404:
-        print(f"Repository not found or already deleted. Status code: {response.status_code}")
+        print(f'Ошибка: {response.status_code}, не найден репозиторий')
     else:
-        print(f"Failed to delete repository. Status code: {response.status_code}")
+        print(f'Ошибка: {response.status_code}')
 
-def get_repo(git_token):
-    
-    headers = {
-       "Authorization": f"token {git_token}",
-       "Accept": "application/vnd.github.v3+json"
-    }  
-    
-    response = requests.get(url_get_list_repo, headers=headers)
-    
-    if response.status_code == 200:
-        repos = response.json()
-        repo_names = [repo['name'] for repo in repos]
-        print(repo_names)
-        return repo_names
-    else:
-        print(f"Ошибка: {response.status_code}")
-        return None
-    
-get_repo(git_token)   
-new_repo(git_token, git_repo) 
-get_repo(git_token)    
-delete_repo(git_token)
-get_repo(git_token) 
